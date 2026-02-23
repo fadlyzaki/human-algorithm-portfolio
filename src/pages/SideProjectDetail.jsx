4960968d3c4087fd4e3836b3f5811984d4023a18
Fadly Zaki <fadly.uzzaki@gmail.com>
2026-02-24 01:25:51 +0700

    fix: Use `activeTitle` for the Navbar title in the SideProjectDetail page instead of `project.id`.

--- src/pages/SideProjectDetail.jsx
+++ src/pages/SideProjectDetail.jsx
@@ -162,7 +162,7 @@ const SideProjectDetail = () => {
          <div className="fixed inset-0 pointer-events-none z-0 bg-[radial-gradient(circle_at_center,transparent_0%,var(--bg-void)_120%)]"></div>
 
          {/* --- NAVIGATION SYSTEM --- */}
-         <Navbar onOpenMenu={() => setIsMenuOpen(true)} title={project.id} backPath="/side-projects" />
+         <Navbar onOpenMenu={() => setIsMenuOpen(true)} title={activeTitle} backPath="/side-projects" />
 
          {/* <NavigationMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} /> */}
 
